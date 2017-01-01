ec634d98146dfd88f630dbf91e2264c476ff519e
Andrew Oberstar <ajoberstar@gmail.com>
2016-12-31 18:07:35 -0600

    refactoring to update to latest

--- README.md
+++ README.md
@@ -1,97 +1,10 @@
 # gradle-defaults
 
-**NOTE:** Consider this an experimental plugin. It works for my use case, but I don't consider any of it stable.
+**NOTE:** This is meant to meet my own personal use cases for default settings.
 
 Plugin providing opinionated defaults for Gradle builds.
 
-[![Build Status](https://travis-ci.org/ajoberstar/gradle-defaults.png?branch=master)](https://travis-ci.org/ajoberstar/gradle-defaults)
-[ ![Download](https://api.bintray.com/packages/ajoberstar/gradle-plugins/org.ajoberstar%3Agradle-defaults/images/download.svg) ](https://bintray.com/ajoberstar/gradle-plugins/org.ajoberstar%3Agradle-defaults/_latestVersion)
-[![Stories in Ready](https://badge.waffle.io/ajoberstar/gradle-defaults.png?label=ready&title=Ready)](https://waffle.io/ajoberstar/gradle-defaults)
-
-## Core Functionality
-
-Currently, the defaults aren't terribly easy to override or pick and choose from. Please submit an issue if you find anything you would like to be more configurable.
-
-- Applies `organize-imports` plugin.
-- Applies `org.ajoberstar.github-pages` plugin. Configures to use:
-	- Extension's `vcsWriteUrl`.
-	- Publish content from `src/gh-pages`.
-- If `java` plugin applied:
-	- Apply `jacoco` plugin.
-	- Apply `sonar-runner` plugin.
-	- Apply `eclipse` plugin.
-	- Apply `idea` plugin.
-	- Configure `org.ajoberstar.github-pages` to deploy `docs/javadoc`.
-	- Add tasks and artifacts for a `-sources.jar` and `-javadoc.jar`.
-- If `groovy` plugin applied:
-	- Configure `org.ajoberstar.github-pages` to deploy `docs/groovydoc`
-	- Adds tasks and artifacts for `-groovydoc.jar`.
-- If `scala` plugin applied:
-	- Configure `org.ajoberstar.github-pages` to deploy `docs/scaladoc`.
-	- Add tasks and artifacts for `-scaladoc.jar`.
-- Applies `license` plugin.
-	- Uses license header from `gradle/HEADER`
-		- Substitutes extensions' `copyrightYears` for `${year}` in header.
-	- Enforces strict checking.
-	- Disables normal mappings and sets `groovy` and `java` files to use slashstar style instead of default javadoc style.
-	- Excludes properties files from checks.
-- Applies `org.ajoberstar.release-opinion` plugin.
-	- Configures to use the project's root dir as the repository.
-	- Configures the `release` task to depend on `clean`, `build` and `publishGhPages`.
-	- If `com.jfrog.bintray` applied:
-		- Configure the `release` task to depend on `bintrayUpload`.
-- Configures ordering rules for tasks:
-	- All tasks should run after `clean`.
-	- All tasks in the `publishing` group should run after `build`.
-- If `maven-publish` applied:
-	- Configure the `main` publication to include any of the following that exist:
-		- `.jar`
-		- `-sources.jar`
-		- `-javadoc.jar`
-		- `-groovydoc.jar`
-		- `-scaladoc.jar`
-	- Configures the POM with information from project and extension.
-- If `com.jfrog.bintray` applied and `bintrayUser` and `bintrayKey` properties are set on the project:
-	- If the extension's `bintrayLabels` includes `gradle`:
-		- Set the `gradle-plugin` attribute to support the Gradle plugin portal for any plugin IDs in `src/main/resources`.
-	- Publishes the `main` publication.
-	- Configures settings using the project and extension.
-
-## Configuration
-
-```groovy
-apply plugin: 'java'
-apply plugin: 'maven-publish'
-
-plugins {
-	id 'com.jfrog.bintray' version '<version>'
-	id 'org.ajoberstar.defaults' version '<version>'
-}
-
-group = 'my.group'
-description = 'description of project'
-
-defaults {
-	id = 'github and bintray user or org ID'
-
-	// if using an organization in github and bintray
-	orgName = 'friendly name of org'
-	orgUrl = 'website of org'
-
-	bintrayRepo = 'my repo'
-	bintrayLabels = ['label1', 'label2']
-
-	// optional
-	developers = [
-		[id: 'github id', name: 'your name', email: 'your email']
-	]
-
-	// optional
-	contributors = [
-		[id: 'github id', name: 'their name', email: 'their email']
-	]
-
-	// used by license plugin
-	copyrightYears = '2013-2014'
-}
-```
+[![Bintray](https://api.bintray.com/packages/ajoberstar/gradle-plugins/org.ajoberstar%3Agradle-defaults/images/download.svg)](https://bintray.com/ajoberstar/gradle-plugins/org.ajoberstar%3Agradle-defaults/_latestVersion)
+[![Travis](https://img.shields.io/travis/ajoberstar/gradle-defaults.svg?style=flat-square)](https://travis-ci.org/ajoberstar/gradle-defaults)
+[![Quality Gate](https://sonarqube.ajoberstar.com/api/badges/gate?key=org.ajoberstar:gradle-defaults)](https://sonarqube.ajoberstar.com/dashboard/index/org.ajoberstar:gradle-defaults)
+[![GitHub license](https://img.shields.io/github/license/ajoberstar/gradle-defaults.svg?style=flat-square)](https://github.com/ajoberstar/gradle-defaults/blob/master/LICENSE)
--- src/main/groovy/org/ajoberstar/gradle/defaults/DefaultsPlugin.groovy
+++ src/main/groovy/org/ajoberstar/gradle/defaults/DefaultsPlugin.groovy
@@ -15,10 +15,6 @@
  */
 package org.ajoberstar.gradle.defaults
 
-import nl.javadude.gradle.plugins.license.License
-
-import org.ajoberstar.grgit.Grgit
-
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.Task
@@ -28,26 +24,26 @@ import org.gradle.api.tasks.bundling.Jar
 class DefaultsPlugin implements Plugin<Project> {
 
 	void apply(Project project) {
-		DefaultsExtension extension = project.extensions.create('defaults', DefaultsExtension, project)
-		project.plugins.apply('org.ajoberstar.organize-imports')
-
-		addGhPagesConfig(project, extension)
-		addReleaseConfig(project, extension)
+    if (project.rootProject != project) {
+      throw new GradleException('org.ajoberstar.defaults must only be applied to the root project')
+    }
+		addGit(project)
+    addSonar(project)
+		addReleaseConfig(project)
 
 		project.allprojects { prj ->
-			addJavaConfig(prj, extension)
-			addGroovyConfig(prj, extension)
-			addScalaConfig(prj, extension)
-			addLicenseConfig(prj, extension)
-			addMavenPublishingConfig(prj, extension)
-            addBintrayPublishingConfig(prj, extension)
-			addOrderingRules(prj, extension)
+      addSpotless(project)
+			addJavaConfig(prj)
+			addGroovyConfig(prj)
+			addMavenPublishingConfig(prj)
+      addBintrayPublishingConfig(prj)
+			addOrderingRules(prj)
 		}
 	}
 
 
-	private void addGhPagesConfig(Project project, DefaultsExtension extension) {
-		project.plugins.apply('org.ajoberstar.github-pages')
+	private void addGit(Project project) {
+		project.plugins.apply('org.ajoberstar.git-publish')
 
 		def addOutput = { task ->
 			project.githubPages.pages.from(task.outputs.files) {
@@ -58,25 +54,45 @@ class DefaultsPlugin implements Plugin<Project> {
 		project.allprojects { prj ->
 			prj.plugins.withId('java') { addOutput(prj.javadoc) }
 			prj.plugins.withId('groovy') { addOutput(prj.groovydoc) }
-			prj.plugins.withId('scala') { addOutput(prj.scaladoc) }
 		}
 
-		project.afterEvaluate {
-			project.githubPages {
-				repoUri = extension.vcsWriteUrl
-				pages {
-					from 'src/gh-pages'
-				}
+		project.gitPublish {
+			contents {
+				from 'src/gh-pages'
 			}
 		}
 	}
 
-	private void addJavaConfig(Project project, DefaultsExtension extension) {
+  private void addSonar(Project project) {
+    project.plugins.apply('org.sonarqube')
+    project.sonarqube {
+      properties {
+          property 'sonar.projectVersion', version.toString().split('-')[0]
+      }
+    }
+  }
+
+  private void addSpotless(Project project) {
+    project.plugins.apply('com.diffplug.gradle.spotless')
+    project.spotless {
+      java {
+        target 'src/**/*.java'
+        googleJavaFormat()
+        licenseHeaderFile project.rootProject.file('gradle/HEADER')
+      }
+      format 'groovy', {
+        target 'src/**/*.groovy'
+        trimTrailingWhitespace()
+        indentWithSpaces(2)
+        endWithNewline()
+        licenseHeaderFile project.rootProject.file('gradle/HEADER')
+      }
+    }
+  }
+
+	private void addJavaConfig(Project project) {
 		project.plugins.withId('java') {
 			project.plugins.apply('jacoco')
-			project.plugins.apply('sonar-runner')
-			project.plugins.apply('eclipse')
-			project.plugins.apply('idea')
 
 			Task sourcesJar = project.tasks.create('sourcesJar', Jar)
 			sourcesJar.with {
@@ -89,78 +105,34 @@ class DefaultsPlugin implements Plugin<Project> {
 				classifier = 'javadoc'
 				from project.tasks.javadoc.outputs.files
 			}
-
-			project.artifacts {
-				archives sourcesJar
-				archives javadocJar
-			}
 		}
 	}
 
-	private void addGroovyConfig(Project project, DefaultsExtension extension) {
+	private void addGroovyConfig(Project project) {
 		project.plugins.withId('groovy') {
 			Task groovydocJar = project.tasks.create('groovydocJar', Jar)
 			groovydocJar.with {
 				classifier = 'groovydoc'
 				from project.tasks.groovydoc.outputs.files
 			}
-
-			project.artifacts {
-				archives groovydocJar
-			}
-		}
-
-	}
-
-	private void addScalaConfig(Project project, DefaultsExtension extension) {
-		project.plugins.withId('scala') {
-			Task scaladocJar = project.tasks.create('scaladocJar', Jar)
-			scaladocJar.with {
-				classifier = 'scaladoc'
-				from project.tasks.scaladoc.outputs.files
-			}
-
-			project.artifacts {
-				archives scaladocJar
-			}
 		}
 	}
 
-	private void addLicenseConfig(Project project, DefaultsExtension extension) {
-		project.plugins.apply('license')
-		project.afterEvaluate {
-			project.license {
-				header = project.rootProject.file('gradle/HEADER')
-				strictCheck = true
-				useDefaultMappings = false
-				mapping 'groovy', 'SLASHSTAR_STYLE'
-				mapping 'java', 'SLASHSTAR_STYLE'
-				ext.year = extension.copyrightYears
-			}
-			project.tasks.withType(License) {
-				exclude '**/*.properties'
-			}
-		}
-	}
-
-	private void addReleaseConfig(Project project, DefaultsExtension extension) {
-		project.plugins.apply('org.ajoberstar.release-opinion')
-		project.release {
-			grgit = Grgit.open(project.file('.'))
-		}
-        def releaseTask = project.tasks.release
-        releaseTask.dependsOn 'publishGhPages'
-        project.allprojects { prj ->
-            prj.plugins.withId('org.gradle.base') {
-                releaseTask.dependsOn prj.clean, prj.build
-            }
-            prj.plugins.withId('com.jfrog.bintray') {
-                releaseTask.dependsOn prj.bintrayUpload
-            }
+	private void addReleaseConfig(Project project) {
+		project.plugins.apply('org.ajoberstar.release-experimental')
+    def releaseTask = project.tasks.release
+    releaseTask.dependsOn 'gitPublishPush'
+    project.allprojects { prj ->
+        prj.plugins.withId('org.gradle.base') {
+            releaseTask.dependsOn prj.clean, prj.build
         }
+        prj.plugins.withId('maven-publish') {
+            releaseTask.dependsOn prj.publish
+        }
+    }
 	}
 
-	private void addOrderingRules(Project project, DefaultsExtension extension) {
+	private void addOrderingRules(Project project) {
 		project.plugins.withId('org.gradle.base') {
 			def clean = project.tasks['clean']
 			project.tasks.all { task ->
@@ -178,7 +150,7 @@ class DefaultsPlugin implements Plugin<Project> {
 		}
 	}
 
-	private void addMavenPublishingConfig(Project project, DefaultsExtension extension) {
+	private void addMavenPublishingConfig(Project project) {
 		project.plugins.withId('maven-publish') {
 			project.publishing {
 				publications {
@@ -192,120 +164,17 @@ class DefaultsPlugin implements Plugin<Project> {
 						project.plugins.withId('groovy') {
 							artifact project.groovydocJar
 						}
-
-						project.plugins.withId('scala') {
-							artifact project.scaladocJar
-						}
-
-						pom.withXml {
-							asNode().with {
-								appendNode('name', project.name)
-								appendNode('description', project.description)
-								appendNode('url', extension.siteUrl)
-								if (extension.orgName) {
-									appendNode('organization').with {
-										appendNode('name', extension.orgName)
-										appendNode('url', extension.orgUrl)
-									}
-								}
-								appendNode('licenses').with {
-									appendNode('license').with {
-										appendNode('name', extension.licenseName)
-										appendNode('url', extension.licenseUrl)
-									}
-								}
-								if (extension.developers) {
-									appendNode('developers').with { node ->
-										extension.developers.each { developer ->
-											node.appendNode('developer').with {
-												appendNode('id', developer.id)
-												appendNode('name', developer.name)
-												appendNode('email', developer.email)
-											}
-										}
-									}
-								}
-								if (extension.contributors) {
-									appendNode('contributors').with { node ->
-										extension.contributors.each { contributor ->
-											node.appendNode('contributor').with {
-												appendNode('id', contributor.id)
-												appendNode('name', contributor.name)
-												appendNode('email', contributor.email)
-											}
-										}
-									}
-								}
-								appendNode('scm').with {
-									appendNode('connection', "scm:git:${extension.vcsReadUrl}")
-									appendNode('developerConnection', "scm:git:${extension.vcsWriteUrl}")
-									appendNode('url', extension.siteUrl)
-								}
-							}
-						}
 					}
 				}
 				repositories {
 					mavenLocal()
-				}
-			}
-		}
-	}
-
-	private void addBintrayPublishingConfig(Project project, DefaultsExtension extension) {
-		project.plugins.withId('com.jfrog.bintray') {
-			if (project.hasProperty('bintrayUser') && project.hasProperty('bintrayKey')) {
-				project.afterEvaluate {
-					def bintrayAttributes = [:]
-					if (extension.bintrayLabels.contains('gradle')) {
-						def pluginIds = project.fileTree(
-							dir: 'src/main/resources/META-INF/gradle-plugins',
-							include: '*.properties').collect { file -> file.name[0..(file.name.lastIndexOf('.') - 1)] }
-						bintrayAttributes = ['gradle-plugin': pluginIds.collect { "${it}:${project.group}:${project.name}" }]
-					}
-
-					project.bintray {
-						user = project.bintrayUser
-						key = project.bintrayKey
-
-						publications = ['main']
-
-						publish = true
-						pkg {
-							if (extension.orgName) {
-								userOrg = extension.id
-							}
-							repo = extension.bintrayRepo
-							name = extension.bintrayPkg
-							desc = project.description
-							websiteUrl = extension.siteUrl
-							issueTrackerUrl = extension.issuesUrl
-							vcsUrl = extension.vcsReadUrl
-							licenses = [extension.licenseKey]
-							labels = extension.bintrayLabels
-							publicDownloadNumbers = true
-							version {
-								vcsTag = "v${project.version}"
-								attributes = bintrayAttributes
-								gpg {
-									sign = true
-									if (project.hasProperty('gpgPassphrase')) {
-										passphrase = project.gpgPassphrase
-									}
-								}
-                                if (extension.bintrayToCentral) {
-    								mavenCentralSync {
-								    	if (project.hasProperty('sonatypeUsername')) {
-								    		user = project.sonatypeUsername
-								    	}
-								    	if (project.hasProperty('sonatypePassword')) {
-								    		password = project.sonatypePassword
-								    	}
-								    }
-                                }
-							}
-						}
-					}
+          maven {
+            url = "https://api.bintray.com/maven/ajoberstar/maven/${project.rootProject.name}/;publish=1"
+            credentials {
+              username = System.env['BINTRAY_USER']
+              password = System.env['BINTRAY_KEY']
+            }
+          }
 				}
 			}
 		}
