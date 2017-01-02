c47b1d4bd650babff23b72e83a0854a4dd0e44cc
Andrew Oberstar <ajoberstar@gmail.com>
2017-01-02 15:09:54 -0600

    temporarily disable old license tasks

--- src/main/groovy/org/ajoberstar/gradle/defaults/DefaultsPlugin.groovy
+++ src/main/groovy/org/ajoberstar/gradle/defaults/DefaultsPlugin.groovy
@@ -16,6 +16,7 @@
 package org.ajoberstar.gradle.defaults
 
 import org.gradle.api.Plugin
+import org.gradle.api.GradleException
 import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.api.publish.maven.MavenPublication
