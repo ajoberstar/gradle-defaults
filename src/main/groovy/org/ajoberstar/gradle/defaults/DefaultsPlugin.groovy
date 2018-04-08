91364517e5c04ae98218a658cbf1a0695e57cbe9
Andrew Oberstar <ajoberstar@gmail.com>
2018-04-08 14:27:35 -0500

    Safety checks on tag create

--- src/main/groovy/org/ajoberstar/gradle/defaults/DefaultsPlugin.groovy
+++ src/main/groovy/org/ajoberstar/gradle/defaults/DefaultsPlugin.groovy
@@ -70,6 +70,20 @@ class DefaultsPlugin implements Plugin<Project> {
       preRelease = stageFromProp('alpha', 'beta', 'rc', 'final')
     }
 
+    // safety checks before releasing
+    rootProject.tasks.reckonTagCreate.doFirst {
+      if (rootProject.grgit.branch.current().name != 'master') {
+        throw new IllegalStateException('Can only release from master.')
+      }
+      if (!rootProject.version.toString().contains('-')) {
+        def head = grgit.head()
+        def tagsOnHead = grgit.tag.list().findAll { it.commit == head }
+        if (!tagsOnHead.find { it.name.startsWith(rootProject.version.toString() + '-rc.')}) {
+          throw new IllegalStateException('Must release an rc before making a final.')
+        }
+      }
+    }
+
     // push tags before tag is pushed
     rootProject.tasks.reckonTagPush.dependsOn 'gitPublishPush'
 
