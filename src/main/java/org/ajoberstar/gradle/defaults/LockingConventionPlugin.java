49996f9c2f2ff6a3a7a83f741928a95c4ed666ee
Andrew Oberstar <andrew@ajoberstar.org>
2023-04-22 09:10:32 -0500

    Lock all configurations

--- src/main/java/org/ajoberstar/gradle/defaults/LockingConventionPlugin.java
+++ src/main/java/org/ajoberstar/gradle/defaults/LockingConventionPlugin.java
@@ -2,36 +2,22 @@ package org.ajoberstar.gradle.defaults;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
-import org.gradle.api.tasks.SourceSetContainer;
 
 public class LockingConventionPlugin implements Plugin<Project> {
   @Override
   public void apply(Project project) {
-    project.getPluginManager().apply("java-base");
-    var sourceSets = project.getExtensions().getByType(SourceSetContainer.class);
-
-    // enable locking on each source sets compileClasspath and runtimeClasspath
-    sourceSets.configureEach(sourceSet -> {
-      project.getConfigurations().named(sourceSet.getCompileClasspathConfigurationName(), conf -> {
-        conf.getResolutionStrategy().activateDependencyLocking();
-      });
-      project.getConfigurations().named(sourceSet.getRuntimeClasspathConfigurationName(), conf -> {
-        conf.getResolutionStrategy().activateDependencyLocking();
-      });
+    project.dependencyLocking(locking -> {
+      locking.lockAllConfigurations();
     });
 
-    // task to resolve each source sets compileClasspath and runtimeClasspath
     project.getTasks().register("lock", task -> {
-      task.doFirst(t -> {
+      task.doLast(t -> {
         assert project.getGradle().getStartParameter().isWriteDependencyLocks();
 
-        sourceSets.configureEach(sourceSet -> {
-          project.getConfigurations().named(sourceSet.getCompileClasspathConfigurationName(), conf -> {
-            conf.resolve();
-          });
-          project.getConfigurations().named(sourceSet.getRuntimeClasspathConfigurationName(), conf -> {
-            conf.resolve();
-          });
+        project.getConfigurations().all(configuration -> {
+          if (configuration.isCanBeResolved()) {
+            configuration.resolve();
+          }
         });
       });
     });
